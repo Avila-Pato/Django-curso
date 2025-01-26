9fcea1483832035458028b934afd6532057ab4c8
Avila-Pato <p.avilaf1998@gmail.com>
2025-01-26 15:43:31 -0300

    Add index view and update URL patterns for hello view

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
 
 # Create your views here.
-def hello(request):
+
+def index(request):
+    return HttpResponse("<h1> IndexPage</h1>")
+
+def hello(request, username):
     return HttpResponse("<h1> Hello, World! </h1>")
 
 def about(request):
