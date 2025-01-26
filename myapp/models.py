909c0376cc4c7f90f0c1733f2e0c713c22fa0602
Avila-Pato <p.avilaf1998@gmail.com>
2025-01-26 14:50:56 -0300

    Add Project and Task models with migrations

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here. para la tabla de la base de datos
+# ahi que conectarlo con settings.py en INSTALLED_APPS
+class Project(models.Model):
+    name = models.CharField(max_length=200)
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(),
+    # CASCADE: si se elimina un proyecto se eliminan todas las tareas
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
